a9bce512b8fda783b32d345a05850af1e746c80b
Sohomz <sohomd088@gmail.com>
2025-01-29 14:41:46 +0530

    first comit with draft page

---
+++ CraftyKoKo/src/components/Body.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div className="mt-30">
+      <h1 className="flex justify-center">Body</h1>
+    </div>
+  );
+};
+
+export default Body;
