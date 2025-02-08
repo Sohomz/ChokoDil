4cb29123a8427ab796e15cba8bf4b6cfb6607741
Sohomz <sohomd088@gmail.com>
2025-02-08 14:00:58 +0530

    did something in body addin card dummy

--- CraftyKoKo/src/components/Body.jsx
+++ CraftyKoKo/src/components/Body.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Card from "./Card";
+import useFetchCakeCard from "../utils/useFetchCakeCard";
 
 const Body = () => {
+  const fetchedCakeData = useFetchCakeCard();
   return (
     <div className="mt-30">
-      <h1 className="flex justify-center">Body</h1>
+      <h1 className="flex justify-center">
+        <Card />
+      </h1>
     </div>
   );
 };
